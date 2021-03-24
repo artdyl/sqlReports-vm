f6dca56ea615e611d589969d31ae3538d098a264
artdyl <dyl.artem@gmail.com>
2021-03-24 17:37:47 +0200

    Update bootstrap_ansible.sh

--- scripts/bootstrap_ansible.sh
+++ scripts/bootstrap_ansible.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo yum -y update
-sudo yum -y install ansible.noarch
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum -y install ansible
 sudo yum -y install git
-
